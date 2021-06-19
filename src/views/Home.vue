e194de05dae3b46c5dd7840df8e4b483532be560
AstolfoProto <tokiya.kagami@gmail.com>
2021-06-19 15:25:48 +0800

    add dialog events

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,20 +28,21 @@
         </el-form-item>
       </el-form>
     </div>
+
     <el-pagination
       :current-page="getPagination.currentPage"
       :page-size="25"
       layout="total, prev, pager, next"
       :total="getPagination.totalNum"
       @current-change="pageChange"
-    >
-     </el-pagination>
+    />
+     
     <div class="row horizontal wrap" data-row-count="5">
       <div v-for="(item, i) in result" :key="i" class="data_row" data-inset="0.5rem">
         <div class="country_items">
           <span class="row horizontal v_center" data-border-bottom="light-gray">
             <img :src="`https://www.countryflags.io/${item.alpha2Code}/shiny/64.png`">
-            <b>{{ item.name }}</b>
+            <b @click="openDialog(item)">{{ item.name }}</b>
           </span>
           <div class="row horizontal v_center" data-space-vertical="0.5rem">
             <span class="tags">{{ item.alpha2Code }}</span>
@@ -55,6 +56,70 @@
         </div>
       </div>
     </div>
+    <el-dialog
+      :title="dialogInfo.name"
+      :visible.sync="showDialog"
+      width="50%"
+      class="el-customize"
+      @close="closeDialog"
+    >
+      <div class="row horizontal space">
+        <img :src="`https://www.countryflags.io/${dialogInfo.alpha2Code}/shiny/64.png`">
+        <div class="row horizontal v_center" data-space-before="1rem">
+           <span class="tags">{{ dialogInfo.alpha2Code }}</span>
+          <span class="tags">{{ dialogInfo.alpha3Code }}</span>
+        </div>
+       </div>
+      <div class="row vertical" :style="{'max-height':'400px', 'overflow-y':'auto'}">
+        <el-form label-width="10rem" label-position="left">
+          <el-form-item label="Name:">{{dialogInfo.name}}</el-form-item>
+          <el-form-item label="Top level domain:">{{dialogInfo.topLevelDomain}}</el-form-item>
+          <el-form-item label="Alpha 2 code:">{{dialogInfo.alpha2Code}}</el-form-item>
+          <el-form-item label="Alpha 3 code:">{{dialogInfo.alpha3Code}}</el-form-item>
+          <el-form-item label="Calling codes:">{{dialogInfo.callingCodes}}</el-form-item>
+          <el-form-item label="Capital:">{{dialogInfo.capital}}</el-form-item>
+          <el-form-item label="Alt Spellings:">{{dialogInfo.altSpellings}}</el-form-item>
+          <el-form-item label="Region:">{{dialogInfo.region}}</el-form-item>
+          <el-form-item label="Sub region:">{{dialogInfo.subregion}}</el-form-item>
+          <el-form-item label="Population:">{{dialogInfo.population}}</el-form-item>
+          <el-form-item label="LatLng:">{{dialogInfo.latlng}}</el-form-item>
+          <el-form-item label="Demonym:">{{dialogInfo.demonym}}</el-form-item>
+          <el-form-item label="Area:">{{dialogInfo.area}}</el-form-item>
+          <el-form-item label="Time zones:">{{dialogInfo.timezones}}</el-form-item>
+          <el-form-item label="Borders:">{{dialogInfo.borders}}</el-form-item>
+          <el-form-item label="Native name:">{{dialogInfo.nativeName}}</el-form-item>
+          
+          <el-form-item label="Numeric code:">{{dialogInfo.numericCode}}</el-form-item>
+          <el-form-item label="Currencies:">
+            <div v-for="(item, i) in dialogInfo.currencies" :key="i">{{`${item.name}(Code: ${item.code}, ${item.symbol})`}}<br></div>
+          </el-form-item>
+          <el-form-item label="Languages:">
+            <div v-for="(item, i) in dialogInfo.languages" :key="i">{{`${item.name}(Native: ${item.nativeName}, ISO 639: ${item.iso639_1}, ${item.iso639_2})`}}<br></div>
+          </el-form-item>
+          <el-form-item label="Translations:">
+            <div v-for="(item, i) in translations" :key="i">
+              {{ item }} <br>
+            </div>
+          </el-form-item>
+          <el-form-item label="Flag:">
+            <div class="row horizontal v_center">
+              <img :src="dialogInfo.flag" style="width: 64px;">
+            </div>
+          </el-form-item>
+          <el-form-item label="regionalBlocs">
+            <div v-for="(item, i) in dialogInfo.regionalBlocs" :key="i">
+              <span>{{ `${item.name}(${item.acronym})` }}<br></span>
+              <span v-if="item.otherNames.length > 0">{{ `Other names: ${item.otherNames}` }}<br></span>
+              <span v-if="item.otherAcronyms.length > 0">{{ `Other acronyms: ${item.otherAcronyms}` }}</span>
+            </div>
+          </el-form-item>
+          <el-form-item label="Cioc">{{dialogInfo.cioc}}</el-form-item>
+        </el-form>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="showDialog = false">Close</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -68,7 +133,10 @@ export default {
   data() {
     return {
       search: '',
-      result: []
+      result: [],
+      showDialog: false,
+      dialogInfo: {},
+      translations: []
     }
   },
 
@@ -104,14 +172,31 @@ export default {
       }
       this.SET_PAGE(obj)
       this.result = [...this.getResult[this.getPagination.currentPage -1]]
-    }
+    },
+    async openDialog(obj) {
+      this.dialogInfo = JSON.parse(JSON.stringify(obj))
+      await this.parseInfoContents()
+      this.showDialog = true
+      console.log('##dialog: ', obj)
+    },
+    parseInfoContents() {
+      Object.keys(this.dialogInfo.translations).map(item => {
+        this.translations.push(`${this.dialogInfo.translations[item]}(${item})`)
+      })
+      const excArr = ['currencies', 'languages', 'translations', 'regionalBlocs']
+      Object.keys(this.dialogInfo).map(item => {
+        if (this.dialogInfo[item] instanceof Array && !excArr.includes(item)) {
+          this.dialogInfo[item] = this.dialogInfo[item].join(', ')
+        }
+      })
+    },
+    closeDialog() {
+      this.dialogInfo = {}
+      this.translations = []
+    },
   },
   created() {
     this.init()
-  },
-
-  mounted() {
-
-  },
+  }
 }
 </script>
